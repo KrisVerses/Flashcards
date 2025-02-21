56769ef7261022c65d5e7b6ea9b0c3903c6a2b30
KrisVerses <krisincode@gmail.com>
2025-02-21 13:35:36 -0600

    feat: hook redux to topics component for state management

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {},
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      state.topics = {
+        [action.payload.id]: { ...action.payload, quizIds: [] },
+      };
+    },
+  },
+});
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
